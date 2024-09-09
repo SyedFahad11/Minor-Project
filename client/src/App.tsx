f1626ce31123b4c5a95a979d38487c8fcb9b0e3c
SyedFahad11 <SYEDHU987@GMAIL.COM>
2024-09-09 08:03:29 +0530

    Added Routes, Landing page, Connect button

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,37 @@
-import UserRegister from "./components/Register"
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { ConnectKitProvider, getDefaultConfig } from "connectkit"
+import { WagmiProvider, createConfig } from "wagmi"
+import { arbitrumSepolia } from "wagmi/chains"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+
+import UserRegister from "./components/Register";
+import LandingPage from "./components/Landing";
+
+const config = createConfig(
+  getDefaultConfig({
+    walletConnectProjectId:"",
+    chains: [arbitrumSepolia],
+    appName: "Supply_Trace",
+  })
+)
+const queryClient = new QueryClient()
 function App() {
 
   return (
     <>
-      <UserRegister/>
+      <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <ConnectKitProvider theme="rounded">
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<LandingPage />} />
+              <Route path="/register" element={<UserRegister />} />
+            </Routes>
+          </BrowserRouter>
+        </ConnectKitProvider>
+        </QueryClientProvider>
+      </WagmiProvider>
     </>
   )
 }
