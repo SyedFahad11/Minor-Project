1f8a3ff5980fcae637f86b381b653e4661473210
shourya <you@example.com>
2024-09-14 18:51:15 +0530

    Ensure source maps are enabled

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,6 +4,9 @@ import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    sourcemap: true, // Ensure source maps are enabled
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
